0dcb1eff620762721bebafe42c596db42eb50a9a
Adrián Morales Martín <githug321@gmail.com>
2017-01-22 18:52:24 +0000

    Add files via upload

---
+++ ReservasVuelo/src/main/webapp/JS/ReservaVuelos.js
@@ -0,0 +1,10 @@
+var ReservaVuelos = function(){
+  /*this.fechaIni = document.getElementById("").value;
+  this.fechaFin = document.getElementById("").value;*/
+}
+ReservaVuelos.prototype.comprobarFechas = function(fechaIni, fechaFin){
+
+}
+ReservaVuelos.prototype.validarCosteMaximo = function(campo){
+
+}
